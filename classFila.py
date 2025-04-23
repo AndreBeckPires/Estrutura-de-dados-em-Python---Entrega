bc9026159466fc936e3396fd8a2d7cd340bb3615
unknown <andrebeckpires@gmail.com>
2025-04-22 21:24:50 -0300

    fixes

--- classFila.py
+++ classFila.py
@@ -3,7 +3,7 @@ class Fila():
         self.objetos = []
 
     def push(self,obj):
-        self.objetos.append(obj)
+        self.objetos.insert(0,obj)
     
     def getSize(self):
         return len(self.objetos)
@@ -13,5 +13,8 @@ class Fila():
 
     def printFila(self):
         print(self.objetos)
+
+    def getProximo(self):
+        return self.objetos[len(self.objetos)-1]
 
     
