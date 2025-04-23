c934ebacbfe76318ff3beea2e888ae0e54f205cf
unknown <andrebeckpires@gmail.com>
2025-04-22 21:38:04 -0300

    sorteando as filas

--- Questão 2.py
+++ Questão 2.py
@@ -1,15 +1,27 @@
 from classFila import Fila
-
+import random
 
 prioridadeBaixa =  Fila()
 prioridadeMedia = Fila()
 prioridadeAlta = Fila()
+prioridadeIdoso = Fila()
 
-prioridadeBaixa.push(0)
-prioridadeBaixa.push(1)
-prioridadeBaixa.push(2)
-prioridadeBaixa.push(3)
+for i in range(0,10):
+    paciente = random.randint(1,4)
+    match paciente:
+        case 1:
+            prioridadeBaixa.push(i)
+        case 2:
+            prioridadeMedia.push(i)
+        case 3:
+            prioridadeAlta.push(i)
+        case 4:
+            prioridadeIdoso.push(i)
+        case _:
+            print("ERROR")
+
+
 prioridadeBaixa.printFila()
-prioridadeBaixa.pop()
-prioridadeBaixa.printFila()
-print(prioridadeBaixa.getProximo())
+prioridadeMedia.printFila()
+prioridadeAlta.printFila()
+prioridadeIdoso.printFila()
